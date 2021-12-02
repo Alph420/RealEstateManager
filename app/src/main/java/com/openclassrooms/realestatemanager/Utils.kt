5bde8ecf7ea0c1d7fa910b4ca392b74cd41e7672
Julien Jennequin <Julienjennequin59123@gmail.com>
2021-12-02 15:53:27 +0100

    IN PROGRESS: - Migrate java class to kotlin class - Refactor xml components - Fix activity in Manifest - Fix compile and target sdk version to 31 - Fix min sdk version to 21 (5.0) - Migrate to viewBinding

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/Utils.kt
@@ -0,0 +1,46 @@
+package com.openclassrooms.realestatemanager
+
+import android.content.Context
+import android.net.wifi.WifiManager
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.math.roundToInt
+
+/**
+ * Created by Julien Jennequin on 02/12/2021 15:35
+ * Project : RealEstateManager
+ **/
+internal object Utils {
+
+    /**
+     * Conversion d'un prix d'un bien immobilier (Dollars vers Euros)
+     * NOTE : NE PAS SUPPRIMER, A MONTRER DURANT LA SOUTENANCE
+     * @param dollars
+     * @return
+     */
+     fun convertDollarToEuro(dollars: Int): Int {
+        return (dollars * 0.812).roundToInt()
+    }
+
+    /**
+     * Conversion de la date d'aujourd'hui en un format plus approprié
+     * NOTE : NE PAS SUPPRIMER, A MONTRER DURANT LA SOUTENANCE
+     * @return
+     */
+    fun getTodayDate(): String? {
+        val dateFormat: DateFormat = SimpleDateFormat("yyyy/MM/dd")
+        return dateFormat.format(Date())
+    }
+
+    /**
+     * Vérification de la connexion réseau
+     * NOTE : NE PAS SUPPRIMER, A MONTRER DURANT LA SOUTENANCE
+     * @param context
+     * @return
+     */
+    fun isInternetAvailable(context: Context): Boolean {
+        val wifi = context.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        return wifi.isWifiEnabled
+    }
+}
