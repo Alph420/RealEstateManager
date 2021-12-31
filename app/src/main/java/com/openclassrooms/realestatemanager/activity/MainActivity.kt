87df239992ca24013a590f5ea36a57a9463c450b
Julien Jennequin <Julienjennequin59123@gmail.com>
2021-12-31 12:09:32 +0100

    IN PROGRESS: - Perma fix for realty picture in list

--- app/src/main/java/com/openclassrooms/realestatemanager/activity/MainActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/activity/MainActivity.kt
@@ -187,12 +187,12 @@ class MainActivity : BaseActivity() {
             { result ->
                 Log.d(TAG, result.toString())
                 realtyList = result
-                updateView()
-                initDetailPart()
                 realtyList.forEach { realty ->
                     disposeBag += mainViewModel.getPictures(realty.id).subscribe(
                         { result ->
                             realty.pictures = result
+                            updateView()
+                            initDetailPart()
                         },
                         { error ->
                             Log.e(TAG, error.message.toString())
