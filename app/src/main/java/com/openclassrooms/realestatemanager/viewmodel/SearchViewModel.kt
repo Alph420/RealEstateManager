90d5c99b92cf6b8dd4fd23963897280d2f5a2039
Julien Jennequin <Julienjennequin59123@gmail.com>
2022-01-13 11:40:08 +0100

    IN PROGRESS: - Fix crash issues in SearchActivity.kt

--- app/src/main/java/com/openclassrooms/realestatemanager/viewmodel/SearchViewModel.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/viewmodel/SearchViewModel.kt
@@ -132,6 +132,6 @@ class SearchViewModel(private val database: AppDatabase) : ViewModel() {
 
                 true
             }
-        }.firstOrError().subscribeOn(Schedulers.io())
+        }.firstOrError()
 
 }
